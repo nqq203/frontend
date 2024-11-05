4fbc015629a07c55b6eb94cd1a072c40dc120264
Ngo Quoc Quy <quy.nqhcm@gmail.com>
2024-11-05 17:23:53 +0700

    update nginx

--- src/api/user.js
+++ src/api/user.js
@@ -9,6 +9,7 @@ export async function register({ username, email, password }) {
   console.log(userInfo);
   try {
     const { data } = await api.post('/user/register', userInfo);
+    console.log(data);
     return data;
   } catch (err) {
     console.log("Error registering user:", err.response || err);
